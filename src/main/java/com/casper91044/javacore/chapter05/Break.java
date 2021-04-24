9f96b9e2c89a79ee0032a32645b147e5f581ed38
casper91044 <casper91044@mail.ru>
2021-04-24 16:40:08 +0300

    End of chapter 5.

---
+++ src/main/java/com/casper91044/javacore/chapter05/Break.java
@@ -0,0 +1,23 @@
+package main.java.com.casper91044.javacore.chapter05;
+
+// Применение оператора break в качестве цивилизованной
+// формы оператора goto
+
+public class Break {
+    public static void main(String[] args) {
+        boolean t = true;
+
+        first:{
+            second:{
+                third:{
+                    System.out.println("Предшествует оператору break");
+                    if (t) break second; // выход из блока second
+                    System.out.println("Этот оператор не будет выполняться");
+                }
+                System.out.println("Этот оператор не будет выполняться");
+
+            }
+            System.out.println("Этот оператор следует за блоком second");
+        }
+    }
+}
